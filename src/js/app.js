70634103c8561b20409bd6d4cb14de272c25435d
frasaleksander <fongreecss@gmail.com>
2020-01-17 10:00:31 +0100

    adding some comments

--- src/js/app.js
+++ src/js/app.js
@@ -3,26 +3,23 @@ import Navigation from "./components/navigation";
 import Parallax from "./components/parallax";
 import Slider from "./components/slider";
 import InViewport from './components/inviewport';
+//scrollspy magic
 import Gumshoe from 'gumshoejs';
 //this is actually just a wrapper around yt-player
 import YoutubePlayer from './components/youtube-player';
+//wrapper for tns - tiny slider
 import TinySlider from "./components/tiny-slider";
 
-//import lazySizes from 'lazysizes';
-
-/** load images that are not in viewport 
- * (they should be marked as they) 
- * after page has completely loaded 
- * */
-window.addEventListener('load', ()=> {
-  document.querySelectorAll('img[data-src]').forEach((el) => {
-     el.src = el.dataset.src;
-  });
-}, false);
+/*
+//use this for crossbrowser support. 
+//was substituted with html attr loading="lazy"
+import lazySizes from 'lazysizes';
+*/
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('body').classList.add('dom-ready');
   document.querySelector('html').classList.remove('no-js');
+  
   new CookieNotice();
   new Navigation();
   new Parallax();
